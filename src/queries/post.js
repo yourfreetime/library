2ea6e425b8b7a8829261d8408a952aed73bd532c
will.tamagi <will.tamagi@gmail.com>
2020-05-16 23:32:17 -0300

    Added LIST_POSTS_BY_LOCATION query (#1)
    * feat: Added filter variable to LIST_POSTS_FEED query
    
    * feat: Added new query for list post with geoLocation
    
    * fix: Adjusted pattern of added queries

--- src/queries/post.js
+++ src/queries/post.js
@@ -1,8 +1,34 @@
 import gql from 'graphql-tag';
 
 export const LIST_POSTS_FEED = gql`
-  query listPostsFeed {
-    listPostsFeed {
+  query listPostsFeed($search: String) {
+    listPostsFeed(filter: { search: $search }) {
+      id
+      text
+      dateCreated
+      author {
+        id
+        name
+        picture
+      }
+      likes {
+        date
+        user {
+          id
+        }
+      }
+      comments {
+        dateCreated
+      }
+    }
+  }
+`;
+
+export const LIST_POSTS_BY_LOCATION = gql`
+  query listPostsByLocation($latitude: Float!, $longitude: Float!) {
+    listPostsByLocation(
+      filter: { latitude: $latitude, longitude: $longitude }
+    ) {
       id
       text
       dateCreated
@@ -57,5 +83,5 @@ export const GET_POST = gql`
 
 export default {
   LIST_POSTS_FEED,
-  GET_POST
+  GET_POST,
 };
